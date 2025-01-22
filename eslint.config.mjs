27af80f4480f91585bfa7a341ddab9a78ab9788d
Lee Whittaker <lee@petersenwest.com>
2025-01-22 11:38:51 -0700

    ENZ-9 Optimize ESLint and Prettier config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 import structuredClone from 'structured-clone';
 globalThis.structuredClone = structuredClone;
@@ -10,10 +11,25 @@ globalThis.structuredClone = structuredClone;
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    ignores: ["node_modules/*", "build/*", "dist/*", "coverage/*"]
+    ignores: ["node_modules/*", "build/*", "dist/*", "coverage/*"],
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      'react': pluginReact,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      }
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    }
   },
-  { languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ];
